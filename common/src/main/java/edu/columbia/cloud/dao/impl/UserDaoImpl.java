b7fcbe24d584e73b9a0c2880978ca84487031c89
diwakar21 <diwakar.mahajan@gmail.com>
2014-12-22 14:05:52 -0500

    UserDaoImpl

--- common/src/main/java/edu/columbia/cloud/dao/impl/UserDaoImpl.java
+++ common/src/main/java/edu/columbia/cloud/dao/impl/UserDaoImpl.java
@@ -1,8 +1,15 @@
 package edu.columbia.cloud.dao.impl;
 
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+
+import org.neo4j.shell.util.json.JSONException;
 
 import edu.columbia.cloud.dao.UserDao;
 import edu.columbia.cloud.db.neo4j.Neo4jUtils;
@@ -19,38 +26,33 @@ public class UserDaoImpl implements UserDao {
 	
     @Override
     public boolean createUser(User user) {
-    	return createUser(user, 1);
-    }
-    
-    public boolean createUser(User user, int degree){
     	Map<String, Object> propMap = new HashMap<String, Object>();
     	propMap.put("id", user.getId());
     	propMap.put("email", user.getEmail());
     	propMap.put("name", user.getName());
     	propMap.put("gender", user.getGender());
-    	propMap.put("dob", user.getDob().getTime());
+    	Date dob = user.getDob();
+    	if(dob!=null)
+    		propMap.put("dob",dob.getTime());
     	String nodeUrl = neo4j.createNode(propMap);
     	if(nodeUrl==null)
     		return false;
     	neo4j.addLabels(nodeUrl, USER_TYPE);
     	
     	//adding skills
-    	Map<Skill, Long> skillMap = user.getSkillMap();
-    	Iterator<Entry<Skill, Long>> iterator = skillMap.entrySet().iterator();
-    	while (iterator.hasNext()) {
-			Map.Entry<edu.columbia.cloud.models.Skill, java.lang.Long> entry = (Map.Entry<edu.columbia.cloud.models.Skill, java.lang.Long>) iterator
-					.next();
-			
-			//check if skill exists
-			Skill skill = entry.getKey();
-			String skillURL = neo4j.getNodeUrlByName(skill.getName());
-			if(skillURL == null){
-				if(!createSkill(skill))
-					return false;
-			}
-				
-			//adding relationship
-			neo4j.addRelationship(nodeUrl, skillURL, USER_SKILL_RELATIONSHIP , entry.getValue());
+    	 List<Skill> skillList = user.getSkillList();
+    	 
+    	 for (Skill skill : skillList) {
+ 			String skillURL = neo4j.getNodeUrlByName(skill.getName());
+ 			if(skillURL == null){
+ 				skillURL = createSkill(skill);
+ 				if(skillURL == null)
+ 					return false;
+ 			}
+ 				
+ 			//adding relationship
+ 			neo4j.addRelationship(nodeUrl, skillURL, USER_SKILL_RELATIONSHIP , USER_SKILL_RELATIONSHIP_PARAM,skill.getLevel());
+ 		
 		}
     	
     	//adding friends
@@ -66,32 +68,37 @@ public class UserDaoImpl implements UserDao {
 		}   	
     	return true;
     }
-
     
-    private boolean createSkill(Skill skill){
+    public String createSkill(Skill skill){
     	Map<String, Object> propMap = new HashMap<String, Object>();
     	propMap.put("id", skill.getId());
     	propMap.put("name", skill.getName());
     	propMap.put("category", skill.getCategory());;
     	String nodeUrl = neo4j.createNode(propMap);
     	if(nodeUrl==null)
-    		return false;
+    		return null;
     	neo4j.addLabels(nodeUrl, SKILL_TYPE);
-    	return true;
+    	
+    	return nodeUrl;
     }
     
     
-    @Override
-    public User fetchUser(String userId) {
+    public User fetchUser(String userId, int level) {
+    	try{
     	HashMap<String,Object> userProp = neo4j.getNodeById(userId);
     	Iterator<Entry<String, Object>> iterator = userProp.entrySet().iterator();
-    	User user = new User((String)userProp.get("id"), (String)userProp.get("name"));
-    	user.setDob(new Date(Long.parseLong((String)userProp.get("dob"))));
-
-    	user.setGender((String)userProp.get("gender"));
-    	user.setEmail((String)userProp.get("email"));
+    	Object id = userProp.get("id");
+    	Object name = userProp.get("name");
+    	//String s=name==null?"":(String)name;
+    	User user = new User((String)id, name==null?"":(String)name);
+    	Object dob = userProp.get("dob");
+    	if(dob!=null)
+    	user.setDob(new Date(Long.parseLong((String)dob)));
+    	Object gender = userProp.get("gender");
+    	Object email = userProp.get("email");
+    	user.setGender(gender==null?"":(String)gender);
+    	user.setEmail(email==null?"":(String)email);
     	List<User> userList = new ArrayList<User>();
-
     	while (iterator.hasNext()) {
 			Map.Entry<java.lang.String, java.lang.Object> entry = (Map.Entry<java.lang.String, java.lang.Object>) iterator
 					.next();
@@ -99,19 +106,96 @@ public class UserDaoImpl implements UserDao {
 			Object value = entry.getValue();
 			
 		}
-
-    	return null;
+    	//add skills
+    	 try {
+			Map<String, Object> neighborsDeatilsOverRelation = neo4j.getNeighborsDeatilsOverRelation(userId, 1, "has");
+			Iterator<Entry<String, Object>> iterator2 = neighborsDeatilsOverRelation.entrySet().iterator();
+			while (iterator2.hasNext()) {
+				Map.Entry<java.lang.String, java.lang.Object> entry = (Map.Entry<java.lang.String, java.lang.Object>) iterator2
+						.next();
+				Map<String, Object> skillMap =(Map<String, Object>)entry.getValue();
+				Skill skill = new Skill();
+				skill.setCategory((String)skillMap.get("category"));
+				skill.setId((String)skillMap.get("id"));
+				skill.setName((String)skillMap.get("name"));			
+				
+				//Adding strength part
+				Map<String, Object> map =new HashMap<String, Object>();
+				map.put("userId", "\""+userId+"\"");
+				map.put("skillId", "\""+skill.getId()+"\"");
+				String query="Match (xyz:Person {id:{userId}})-[r:has]-(skills:Skill {id :{skillId}})  return r.strength";
+				String queryDB = neo4j.queryDB(query, map);
+				Integer strength = Integer.parseInt((String) neo4j.getDataFromColumns(queryDB).get("r.strength").get(0));
+				skill.setLevel(strength);
+				
+				//Match (xyz:Person {name:"XYZ"})-[r:has]-(skills:Skill {id :{skillId}}) return r.strength
+				
+				
+				user.addSkillToList(skill);
+			}
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	 
+    	
+    	
+    	if(level > 0){
+    		level--;
+    		//for each user run fetchUser( friendUserId, level);
+    		List<Object> friendsIds = neo4j.getNeighborsOverRelation(userId, 1, "knows");
+        	for (Object friendsId : friendsIds) {
+    			User fetchUser = fetchUser((String)friendsId, level);
+    			//add friends
+    			user.addConnection(fetchUser);
+    		}
+    	}
+    	
+    	return user;
+    	}catch(Exception e){
+    		System.err.println("User fetch failed");
+    		e.printStackTrace();
+    		return null;
+    	}
     }
-
+    
     @Override
-    public List<User> fetchUsersWithSkill(String skill) {
-        return null;
+    public User fetchUser(String userId) {
+    	return fetchUser(userId, 1);
+    }
+    
+    @Override
+    public List<User> fetchUsersWithSkill(String skillId) {
+    	try{
+    	List<User> users = new ArrayList<User>();
+    	Map<String, Object> map =new HashMap<String, Object>();
+		map.put("skillId", "\""+skillId+"\"");
+		String query="Match (xyz:Person )-[r:has]-(skills) where skills.id={skillId} return xyz.id";
+		String queryDB = neo4j.queryDB(query, map);
+		Map<String, List<Object>> dataFromColumns = neo4j.getDataFromColumns(queryDB);
+		List<Object> list = dataFromColumns.get("xyz.id");
+		for (Object object : list) {
+			User user = fetchUser((String)object);
+			users.add(user);
+		}
+		return users;
+    	}catch(Exception e){
+    		System.err.println("User fetch with skills failed");
+    		e.printStackTrace();
+    		return null;
+    	}
     }
 
 	@Override
 	public boolean addSkill(String userId, Skill skill, int strength) {
 		String userUrl = neo4j.getNodeUrlById(userId);
 		String skillUrl = neo4j.getNodeUrlByName(skill.getName());
+		if(skillUrl == null){
+			skillUrl = createSkill(skill);
+			if(skillUrl == null)
+				return false;
+		}
 		String addRelationship = neo4j.addRelationship(userUrl, skillUrl, USER_SKILL_RELATIONSHIP,USER_SKILL_RELATIONSHIP_PARAM,strength);
 		if(addRelationship==null)
 			return false;
@@ -122,8 +206,8 @@ public class UserDaoImpl implements UserDao {
 	public boolean removeSkill(String userId, String skillId) {
 		// TODO Auto-generated method stub
 		Map<String, Object> map =new HashMap<String, Object>();
-		map.put("userId", userId);
-		map.put("skillId", skillId);
+		map.put("userId", "\""+userId+"\"");
+		map.put("skillId", "\""+skillId+"\"");
 		String query="Match (xyz:Person {id:{userId}})-[r:has]-(skills) where skills.id={skillId} delete r";
 		String queryDB = neo4j.queryDB(query, map);
 		if(queryDB==null)
@@ -136,10 +220,10 @@ public class UserDaoImpl implements UserDao {
 	public boolean updateSkill(String userId, String skillId, int strength) {
 		// TODO Auto-generated method stub
 		Map<String, Object> map =new HashMap<String, Object>();
-		map.put("userId", userId);
-		map.put("skillId", skillId);
+		map.put("userId", "\""+userId+"\"");
+		map.put("skillId", "\""+skillId+"\"");
 		map.put("strength", strength);
-		String query="Match (xyz:Person {id:{userId}})-[r:has]-(skills) where skills.id={skillId} set skills.strength={strength} return skills.strength";
+		String query="Match (xyz:Person {id:{userId}})-[r:has]-(skills) where skills.id={skillId} set r."+USER_SKILL_RELATIONSHIP_PARAM+"={strength} return r."+USER_SKILL_RELATIONSHIP_PARAM;
 		String queryDB = neo4j.queryDB(query, map);
 		if(queryDB==null)
 			return false;
@@ -150,7 +234,7 @@ public class UserDaoImpl implements UserDao {
 	public boolean removeUser(String userId) {
 		// TODO Auto-generated method stub
 		Map<String, Object> map =new HashMap<String, Object>();
-		map.put("userId", userId);
+		map.put("userId", "\""+userId+"\"");
 		String query="Match (xyz:Person {id:{userId}})-[r]-()  delete xyz, r";
 		String queryDB = neo4j.queryDB(query, map);
 		if(queryDB==null)
