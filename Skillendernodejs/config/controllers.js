8e36cb5e7819f2e35b302d8c62664229e1435a3e
Bhavdeep Sethi <sethibhavdeep@gmail.com>
2014-12-22 21:10:20 -0500

    post request for node js

--- Skillendernodejs/config/controllers.js
+++ Skillendernodejs/config/controllers.js
@@ -3,16 +3,69 @@ var http=require('http');
 
 
 var loginPage=function(request,reply){
-    console.log("At Homepage");
+    console.log("Login Homepage: "+request.method);
 	var context={
 		hideNav:true
     }
     reply.view('login.html',context);
 };
 
+var homePagePost=function(request, reply){
+
+    console.log("Home Page:"+request.body);
+    request.body = '';
+    request.on("readable", function(){        
+        while (null !== (request.body += request.read())){}
+    });
+
+    // Do something with it
+    request.on("end", function(){
+        console.log(request.body); //-> POST Parameters as String
+    });
+
+    var options = {
+      host: 'skillender.elasticbeanstalk.com',
+      port: 80,
+      path: '/rest/user/456',
+      //+request.params.id
+      method: 'GET',
+      headers:{
+        contentType:'application/json'  
+      }
+      
+    };
+
+    var responseOut;
+    var req = http.request(options, function(res) {
+      //console.log('STATUS: ' + res.statusCode);
+      //console.log('HEADERS: ' + JSON.stringify(res.headers));
+      res.setEncoding('utf8');
+      var data='';
+      res.on('data', function (chunk) {
+      data += chunk;
+        //console.log('BODY: ' + data);
+        //console.log(typeof  data);
+      });
+        
+        res.on('end', function(){
+        responseOut = JSON.parse(data);
+        //console.log("here");
+        //var name=user.name;
+        
+        //console.log( responseOut.user.name, responseOut.user.id);
+        reply.view('home.html',responseOut.user);
+        });
+      
+    });
+    req.end();
+    //console.log("Test:"+responseOut)
+    
+};
+
+
 var homePage=function(request,reply){
 
-    console.log(request.params.id);
+    //console.log("Home Page:"+request.method);
     var options = {
       host: 'skillender.elasticbeanstalk.com',
       port: 80,
@@ -27,28 +80,28 @@ var homePage=function(request,reply){
 
     var responseOut;
     var req = http.request(options, function(res) {
-      console.log('STATUS: ' + res.statusCode);
-      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      //console.log('STATUS: ' + res.statusCode);
+      //console.log('HEADERS: ' + JSON.stringify(res.headers));
       res.setEncoding('utf8');
 	  var data='';
       res.on('data', function (chunk) {
 	  data += chunk;
-        console.log('BODY: ' + data);
-		console.log(typeof  data);
+        //console.log('BODY: ' + data);
+		//console.log(typeof  data);
       });
 		
 		res.on('end', function(){
 		responseOut = JSON.parse(data);
-		console.log("here");
+		//console.log("here");
 		//var name=user.name;
 		
-		console.log( responseOut.user.name, responseOut.user.id);
+		//console.log( responseOut.user.name, responseOut.user.id);
 		reply.view('home.html',responseOut.user);
 		});
 	  
     });
     req.end();
-	console.log("Test:"+responseOut)
+	//console.log("Test:"+responseOut)
     
 };
 
@@ -79,6 +132,12 @@ module.exports=[
         path:'/home/{id}',
         handler:homePage
     },
+    {
+        method:'POST',
+        path:'/home/{id}',
+        handler:homePagePost
+    },
+
 	{
         method:'GET',
         path:'/userhome',
